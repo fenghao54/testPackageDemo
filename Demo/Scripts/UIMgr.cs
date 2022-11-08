da6ffbcd4ad63a90c2eb690a6ca3cee74eb697ee
fenghao <fenghao@xmiles.cn>
2022-11-08 18:30:20 +0800

    dd

---
+++ Demo/Scripts/UIMgr.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMgr : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Button btn1;
+    public Button btn2;
+    public Button btn3;
+    public Button btn4;
+
+    public  Text txt1;
+    DataMgr datamgr;
+    int testDataNum = 10000;
+
+    int testDataNum2 = 5000;
+    void Start()
+    {
+
+        datamgr = this.transform.GetComponent<DataMgr>();
+       
+        btn1.onClick.AddListener( SetData1 );
+        btn2.onClick.AddListener( SetData2 );
+
+
+        btn3.onClick.AddListener( SetNoBackUps );
+        btn4.onClick.AddListener( SetBackUps );
+    }
+
+    public  void  DebugState(string des ) {
+        txt1.text = des;
+    }
+
+    public void SetData1( ) {
+
+        PlayerData _playerdata = datamgr._playerdata;
+        if( _playerdata != null ) {
+           
+            Dictionary<string, string> dicTemp =  new Dictionary<string, string>();
+            for( int i = 0; i < testDataNum; i++ ) {
+                dicTemp[i.ToString()] = "数据" + i.ToString();
+            }
+            _playerdata.dicTest = dicTemp;
+            Debug.Log( "开始存档1" );
+
+#if UNITY_EDITOR
+        
+           LocalDataUtils.SaveToFile<PlayerData>( _playerdata, this ,null );
+#else
+            LocalDataUtils.SaveBinToFile<PlayerData>( _playerdata, this );
+#endif
+        }
+    }
+
+    public void SetData2() {
+
+        PlayerData _playerdata = datamgr._playerdata;
+        if( _playerdata != null ) {
+           
+            Dictionary<string, string> dicTemp = new Dictionary<string, string>();
+            for( int i = 0; i < testDataNum2; i++ ) {
+                dicTemp[i.ToString()] = "存档" + i.ToString();
+            }
+            _playerdata.dicTest = dicTemp;
+            Debug.Log( "开始存档2" );
+#if UNITY_EDITOR
+            LocalDataUtils.SaveToFile<PlayerData>( _playerdata, this, null);
+#else
+            LocalDataUtils.SaveBinToFile<PlayerData>( _playerdata , this);
+#endif
+        }
+    }
+
+    public void SetNoBackUps() {
+        PlayerData _playerdata = datamgr._playerdata;
+        if( _playerdata != null ) {
+
+            Dictionary<string, string> dicTemp = new Dictionary<string, string>();
+            for( int i = 0; i < 10000; i++ ) {
+                dicTemp[i.ToString()] = "存档" + i.ToString();
+            }
+            _playerdata.dicTest = dicTemp;
+            Debug.Log( "开始存档2" );
+            for( int i = 0; i < 10; i++ ) {
+#if UNITY_EDITOR
+            LocalDataUtils.SaveToFileSec<PlayerData>( _playerdata, this, "noBackUps"+i.ToString() );
+#else
+            LocalDataUtils.SaveBinToFile<PlayerData>( _playerdata , this);
+#endif
+            }
+
+        }
+    }
+
+    public void SetBackUps() {
+        PlayerData _playerdata = datamgr._playerdata;
+        if( _playerdata != null ) {
+
+            Dictionary<string, string> dicTemp = new Dictionary<string, string>();
+            for( int i = 0; i < 10000; i++ ) {
+                dicTemp[i.ToString()] = "存档" + i.ToString();
+            }
+            _playerdata.dicTest = dicTemp;
+            Debug.Log( "开始存档2" );
+            for( int i = 0; i < 10; i++ ) {
+#if UNITY_EDITOR
+                LocalDataUtils.SaveToFile<PlayerData>( _playerdata, this, "BackUps" + i.ToString() );
+#else
+                LocalDataUtils.SaveBinToFile<PlayerData>( _playerdata , this);
+#endif
+            }
+
+        }
+    }
+
+
+}
---
+++ Demo/Scripts/role.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class role : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler 
+ {
+
+    
+
+    public RectTransform roler;
+    public void OnBeginDrag( PointerEventData eventData ) {
+      
+    }
+
+    public void OnDrag( PointerEventData eventData ) {
+        Vector3 pos;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle( roler, eventData.position, eventData.enterEventCamera, out pos );
+        roler.position = pos;
+    }
+
+    public void OnEndDrag( PointerEventData eventData ) {
+      
+    }
+
+    private void OnTriggerEnter2D( Collider2D coll ) {
+        if( coll.tag == "role" ) {
+            Debug.Log( "碰撞发生"+coll.gameObject.name );
+
+        }
+    }
+
+    //private void OnCollisionEnter2D( Collision2D collisionInfo ) {
+    //    Debug.Log( "碰撞发生" );
+    //}
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
