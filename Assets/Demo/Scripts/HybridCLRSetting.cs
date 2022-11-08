60a0c68344a749441ba2f09a5bbc3e2d6fbb2f99
fenghao <fenghao@xmiles.cn>
2022-11-08 18:30:20 +0800

    dd

---
+++ Assets/Demo/Scripts/HybridCLRSetting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class HybridCLRSetting : ScriptableObject {
+    public const string formatAssemblys = "Assets/Public";
+    [Header( "测试一" )]
+    public string[] ddd = new string[] { "dd", "cc", "cb" };
+    [Tooltip( "测试二" )]
+    public string[] hotassemblys = new []{ "vvv" };
+
+
+    public async Task LoadDll() {
+
+    }
+    public async void Start() {
+        await LoadDll();
+    }
+}
---
+++ Assets/Demo/Scripts/PlayerData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class PlayerData 
+{
+    public Dictionary<string, string> dicTest;
+    public int dicNum;   
+
+
+    public void Initdata() {
+        dicTest  =   new Dictionary<string, string>();
+        dicNum = 0;
+    }
+}
